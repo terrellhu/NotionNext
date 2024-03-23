c174364a932fdcfe5e67741c48a36a27eaed1833
hutao <hutao@vip.qq.com>
2024-03-23 19:47:15 +0800

    修改配置

--- blog.config.js
+++ blog.config.js
@@ -22,7 +22,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'skqs', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '成人文学爱好者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://skqsapp.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '成人小说,色酷全书,S酷全书', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
