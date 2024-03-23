7073e11be0812fc3c3b077c955fd5f827b0224d8
hutao <hutao@vip.qq.com>
2024-03-23 20:00:21 +0800

    modify config

--- blog.config.js
+++ blog.config.js
@@ -22,7 +22,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '求生之路', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'coding', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://noteht.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, unreal, game', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, unreal, game,技术分享', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'terrellhu514@gmail.com', // 邮箱地址 例如mail@tangly1024.com
